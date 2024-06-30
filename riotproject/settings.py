e118b547dea9c22b5972af042fcb7eb1bde19b5a
DESKTOP-MT7S43P\kyungil <kyungil05@naver.com>
2024-07-01 00:16:33 +0900

    - 프로젝트 secret key 설정

--- riotproject/settings.py
+++ riotproject/settings.py
@@ -20,7 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y3wndldle&-y5d^s)rgus#lsx)vd_h(&2)9crb3^t=g8o#))rr'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
